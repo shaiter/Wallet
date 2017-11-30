5226ae5734d43df1ac34aaf8fa48e8aef6a3e23e
shaiter <Shiterartyom@gmail.com>
2017-11-30 18:40:56 +0300

    Initial Commit

---
+++ Wallet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wallet
+//
+//  Created by Артём Шайтер on 11/30/17.
+//  Copyright © 2017 Артём Шайтер. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
